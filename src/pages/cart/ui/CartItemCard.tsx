ff4c5a8c6aa1eed77e9beb33aed5f44371e29722
dns-006 <samoylenko_d@itstep.academy>
2025-12-18 13:17:22 +0200

    Saves

--- src/pages/cart/ui/CartItemCard.tsx
+++ src/pages/cart/ui/CartItemCard.tsx
@@ -3,41 +3,48 @@ import type CartItem from "../../../entities/cart/model/CartItem";
 import './CartItemCard.css';
 import { AppContext } from "../../../features/app_context/AppContext";
 
-export default function CartItemCard({cartItem}:{cartItem:CartItem}) {
-    const {cart, setCart} = useContext(AppContext);
+export default function CartItemCard({ cartItem }: { cartItem: CartItem }) {
+    const { cart, setCart } = useContext(AppContext);
 
     const incClick = () => {
         // задача: змінити кількість замовлення у {cartItem} та внести зміни
         // до загального кошику через виклик {setCart}
-
-        let newCart = {... cart};
-         let item = newCart.items.find(ci => ci.product.id == cartItem.product.id);
-         if(item )
-         {
+        if(cartItem.product.stock && cartItem.product.stock <= cartItem.cnt)
+        {
+            return;
+        }
+        let newCart = { ...cart };
+        let item = newCart.items.find(ci => ci.product.id == cartItem.product.id);
+        if (item) {       
             item.cnt += 1;
-         }
-
-         
-         setCart(newCart);  
+            item.price = item.product.price * item.cnt;
+            setCart(newCart);
+        }
     };
     const decClick = () => {
-        let newCart = {... cart};
-        let item = newCart.items.find(ci => ci.product.id == cartItem.product.id);
-        if(item )
+        // задача: реалізувати обмеження: кількість не можна зменшити до 0, а також
+        // збільшити понад {stock} якщо його зазначено
+        if(cartItem.cnt <= 1)
         {
-           item.cnt -= 1;
+            return;
         }
-        setCart(newCart);
+        let newCart = { ...cart };
+        let item = newCart.items.find(ci => ci.product.id == cartItem.product.id);
+        if (item) { 
+            item.cnt -= 1;
+            item.price = item.product.price * item.cnt;
+            setCart(newCart);
+        }
     };
 
     return <div className="row m-3 p-2 cart-item-card">
         <div className="col col-2">
-            <img 
-                src={cartItem.product.imageUrl} 
+            <img
+                src={cartItem.product.imageUrl}
                 alt={cartItem.product.name}
                 className="w-100" />
             <div className="text-center">
-                <button className="btn btn-outline-secondary me-2" onClick={decClick}>-</button> 
+                <button className="btn btn-outline-secondary me-2" onClick={decClick}>-</button>
                 {cartItem.cnt}
                 <button className="btn btn-outline-secondary ms-2" onClick={incClick}>+</button>
             </div>
@@ -49,12 +56,12 @@ export default function CartItemCard({cartItem}:{cartItem:CartItem}) {
         </div>
         <div className="col col-3 text-center">
             <h4>{cartItem.price.toMoney()} грн</h4>
-            {cartItem.product.discount && 
+            {cartItem.product.discount &&
                 <div className="text-decoration-line-through">
                     {(cartItem.price + (cartItem.product.discount ?? 0) * cartItem.cnt).toMoney()} грн
-                </div>        
+                </div>
             }
-        </div>    
+        </div>
         <div className="col col-1 text-end"><i className="bi bi-trash3"></i></div>
     </div>;
 }
