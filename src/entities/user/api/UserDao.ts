e13e029bff2d8f7f62a19f267b2901de9727f39f
samoylenko_d <samoylenko_d@28-0>
2026-03-25 11:38:02 +0200

    CORS

--- src/entities/user/api/UserDao.ts
+++ src/entities/user/api/UserDao.ts
@@ -1,8 +1,34 @@
+import Base64 from "../../../shared/base64/Base64";
+import Config from "../../config/Config";
 import type { UserType } from "../model/UserType";
 
 export default class UserDao {
 
     static authenticate(login:string, password:string) : Promise<UserType|null> {
+        return new Promise((resolve, reject) => {
+            fetch(`${Config.backendUrl}/User/SignIn`, {
+                method: "PUT",
+                headers: {
+                    "Authorization": "Basic " + Base64.encode(`${login}:${password}`)
+                }
+            })
+            .then(r => r.json())
+            .then(j => {
+                console.log(j);
+                resolve({
+                    name: "Досвічений користувач",
+                    email: "user@i.ua",
+                    address: "Одеса, Садова 3",
+                    login: "user",
+                    dob: "08 грудня 2025",
+                    imageUrl: "/img/user.jpg"
+                });
+            })
+            .catch(reject);
+        });
+    }
+
+    static authenticateMock(login:string, password:string) : Promise<UserType|null> {
         return new Promise((resolve, _) => {
             setTimeout(
                 () => {
