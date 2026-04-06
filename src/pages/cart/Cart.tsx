6b825664d4d51c9295a401a9a7eae17034988d02
samoylenko_d <samoylenko_d@005-11>
2026-04-06 09:32:51 +0300

    Cart

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -2,9 +2,28 @@ import { useContext } from 'react';
 import './ui/Cart.css';
 import { AppContext } from '../../features/app_context/AppContext';
 import CartItemCard from './ui/CartItemCard';
+import SiteButton from '../../features/buttons/SiteButton';
+import { useNavigate } from 'react-router-dom';
 
 export default function Cart() {
-    const {cart} = useContext(AppContext);
+    const {cart, user, showModal} = useContext(AppContext);
+    const navigate = useNavigate();
+
+    const buyClick = () => {
+        if(!user) {
+            showModal({
+                title: "Кошик",
+                message: "Для оформлення замовлення необхідно війти в систему",
+                buttons: [
+                    {title: "Увійти", callback: () => navigate('/auth')},
+                    {title: "До покупок"},
+                ]
+            });
+        }
+        else {
+            
+        }
+    };
 
     return <>
     <h1 className="display-4">
@@ -15,5 +34,7 @@ export default function Cart() {
         на суму {cart.items.reduce((s,ci) => s + ci.price, 0)} грн
     </h1>
     {cart.items.map(cartItem => <CartItemCard key={cartItem.product.id} cartItem={cartItem} />)}
+
+    {cart.items.length > 0 && <SiteButton text='Придбати' action={buyClick} />}
     </>;
 }
